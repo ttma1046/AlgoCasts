e1cff0849263f9bd49c5a25402bee633080fbabe
Chandler Fang <chandlerfang@gmail.com>
2017-12-17 15:12:07 +1100

    Palindrome changes.

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,31 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    let start = 0;
+    let end = str.length - 1;
 
+    while(start < end) {
+        if (str[start] !== str[end]) {
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    return true;
+    
+/*
+    const reversed = str.split('').reverse().join('');
+
+    return str === reversed;
+    */
+/*
+    return str.split('').every((char, i) => {
+        return char === str[str.length - 1 - i];
+    })*/
+}
+
+palindrome('abba');
 module.exports = palindrome;
