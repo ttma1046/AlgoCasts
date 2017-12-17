7a71b73b80d3fcbf248dbcd18bd1c869cfb7be9f
Chandler Fang <chandlerfang@gmail.com>
2017-12-17 17:15:33 +1100

    Maxchar.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,46 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Common String Questions
+//   What is the most common character in the string ?
+//   Does string A have the same characters as string B (is it an anagram)?
+//   Does the given string have any repeated characters in it ?
+
+function maxChar(str) {
+      const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    /*
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
+    */
+
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+
+    // console.log(charMap);
+
+    // charMap[char] = charMap[char] + 1 || 1;
+  }
+
+    
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+  
+}
 
 module.exports = maxChar;
